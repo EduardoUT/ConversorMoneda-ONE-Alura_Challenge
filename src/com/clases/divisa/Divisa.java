de495578a562a267dde21936efb1a083708b07e5
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-31 19:51:09 -0500

    Agregando atributos de tipo constante para nombre en siglas de divisas

--- src/com/clases/divisa/Divisa.java
+++ src/com/clases/divisa/Divisa.java
@@ -17,6 +17,12 @@ public abstract class Divisa {
     private double tasaCambioLibrasEsterlinas;
     private double tasaCambioYenJapones;
     private double tasaCambioWonSulCoreano;
+    private static final String NOMBRE_DIVISA_MEXICO = "MXN";
+    private static final String NOMBRE_DIVISA_USA = "USD";
+    private static final String NOMBRE_DIVISA_GRAN_BRETANA = "GBP";
+    private static final String NOMBRE_DIVISA_EUROPA = "EUR";
+    private static final String NOMBRE_DIVISA_YEN_JAPON = "JPY";
+    private static final String NOMBRE_DIVISA_WON = "KRW";
 
     /**
      * @return the tasaCambioDolarAmericano
@@ -61,11 +67,54 @@ public abstract class Divisa {
     }
 
     /**
+     * @return the NOMBRE_DIVISA_MEXICO
+     */
+    public static String getNOMBRE_DIVISA_MEXICO() {
+        return NOMBRE_DIVISA_MEXICO;
+    }
+
+    /**
+     * @return the NOMBRE_DIVISA_USA
+     */
+    public static String getNOMBRE_DIVISA_USA() {
+        return NOMBRE_DIVISA_USA;
+    }
+    
+    /**
+     * @return the NOMBRE_DIVISA_EUROPA
+     */
+    public static String getNOMBRE_DIVISA_EUROPA() {
+        return NOMBRE_DIVISA_EUROPA;
+    }
+
+    /**
+     * @return the NOMBRE_DIVISA_GRAN_BRETANA
+     */
+    public static String getNOMBRE_DIVISA_GRAN_BRETANA() {
+        return NOMBRE_DIVISA_GRAN_BRETANA;
+    }
+
+    /**
+     * @return the NOMBRE_DIVISA_YEN_JAPON
+     */
+    public static String getNOMBRE_DIVISA_YEN_JAPON() {
+        return NOMBRE_DIVISA_YEN_JAPON;
+    }
+
+    /**
+     * @return the NOMBRE_DIVISA_WON
+     */
+    public static String getNOMBRE_DIVISA_WON() {
+        return NOMBRE_DIVISA_WON;
+    }
+
+    /**
      * Devuelve el valor de la moneda extranjera con el ajuste a la moneda
      * nacional.
      *
      * @param tasaCambio
      * @param valor
+     * @param valorSeleccionTipoDivisa
      * @return valorTotal - Moneda extranjera a pesos mexicanos.
      */
     public abstract BigDecimal conversionDivisa(double tasaCambio, double valor, String valorSeleccionTipoDivisa);
