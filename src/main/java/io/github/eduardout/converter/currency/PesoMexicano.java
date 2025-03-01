4404f1e0e5eaae2f74eadbb72db4b695bcf60a90
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-03-01 12:35:12 -0600

    Renombrar paquete para divisas

--- src/main/java/io/github/eduardout/converter/currency/PesoMexicano.java
+++ src/main/java/io/github/eduardout/converter/currency/PesoMexicano.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.clases.divisa;
+package io.github.eduardout.converter.currency;
 
 import com.service.ProveedorAPI;
 import java.math.BigDecimal;
