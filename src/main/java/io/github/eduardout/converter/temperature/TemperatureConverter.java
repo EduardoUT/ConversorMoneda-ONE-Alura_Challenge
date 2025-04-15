25a646f261ab734ce562352746030c0222cdecf6
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-04-15 13:40:52 -0600

    Refactorización de clase Temperature

--- src/main/java/io/github/eduardout/converter/temperature/TemperatureConverter.java
+++ src/main/java/io/github/eduardout/converter/temperature/TemperatureConverter.java
@@ -1,158 +1,54 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2025 EduardoUT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package io.github.eduardout.converter.clases.temperatura;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import javax.swing.JOptionPane;
+package io.github.eduardout.converter.temperature;
 
 /**
- * 
- * @author Eduardo Reyes Hernández
+ *
+ * @author EduardoUT
  */
-public class Temperatura {
+public class TemperatureConverter {
 
-    private BigDecimal escalaCelsius;
-    private BigDecimal escalaFarenheit;
-    private BigDecimal escalaKelvin;
-    private static final String SIMBOLO_CELSIUS = "°C";
-    private static final String SIMBOLO_FARENHEIT = "°F";
-    private static final String SIMBOLO_KELVIN = "°K";
+    private CalculableTemperature calculableTemperature;
 
-    public String conversionTemperatura(BigDecimal valorUsuario, String valorSeleccionTemperatura) {
-        BigDecimal res;
-        switch (valorSeleccionTemperatura) {
-            case "Celcius (°C) a Farenheit (°F)":
-                res = valorUsuario.multiply(new BigDecimal("1.8")).add(new BigDecimal("32"));
-                return String.valueOf(res.setScale(2, RoundingMode.FLOOR) + " " + SIMBOLO_FARENHEIT);
-            case "Celcius (°C) a Kelvin (°K)":
-                res = valorUsuario.add(new BigDecimal("273.15"));
-                res.setScale(2, RoundingMode.FLOOR);
-                return String.valueOf(res + " " + SIMBOLO_KELVIN);
-            case "Farenheit (°F) a Celsius (°C)":
-                res = valorUsuario.subtract(new BigDecimal("32")).divide(new BigDecimal("1.8"), 2, RoundingMode.FLOOR);
-                res.setScale(2, RoundingMode.FLOOR);
-                return String.valueOf(res + " " + SIMBOLO_CELSIUS);
-            case "Farenheit (°F) a Kelvin (°K)":
-                res = valorUsuario.subtract(new BigDecimal("32")).multiply(new BigDecimal("5")).divide(
-                        new BigDecimal("9"), 2, RoundingMode.FLOOR).add(new BigDecimal("273.15"));
-                res.setScale(2, RoundingMode.FLOOR);
-                return String.valueOf(res + " " + SIMBOLO_KELVIN);
-            case "Kelvin (°K) a Celsius (°C)":
-                res = valorUsuario.subtract(new BigDecimal("273.15"));
-                res.setScale(2, RoundingMode.FLOOR);
-                return String.valueOf(res + " " + SIMBOLO_CELSIUS);
-            case "Kelvin (°K) a Farenheit (°F)":
-                res = valorUsuario.subtract(new BigDecimal("273.15")).multiply(
-                        new BigDecimal("1.8")).add(new BigDecimal("32"));
-                res.setScale(2, RoundingMode.FLOOR);
-                return String.valueOf(res + " " + SIMBOLO_FARENHEIT);
-            default:
-                return null;
+    public TemperatureConverter(CalculableTemperature calculableTemperature) {
+        validateCalculableTemperature(calculableTemperature);
+        this.calculableTemperature = calculableTemperature;
+    }
+
+    public void setCalculableTemperature(CalculableTemperature calculableTemperature) {
+        validateCalculableTemperature(calculableTemperature);
+        this.calculableTemperature = calculableTemperature;
+    }
+    
+    private void validateCalculableTemperature(CalculableTemperature calculableTemperature) {
+        if(calculableTemperature == null) {
+            throw new IllegalArgumentException("Class instance is null.");
         }
     }
 
-    public BigDecimal conversionTemperaturaAlternativo(BigDecimal valorUsuario, String valorSeleccionTemperatura) {
-        BigDecimal res;
-        switch (valorSeleccionTemperatura) {
-            case "Celcius (°C) a Farenheit (°F)":
-                res = valorUsuario.multiply(new BigDecimal("1.8")).add(new BigDecimal("32"));
-                mensajeResultadoConversion(valorUsuario, res, valorSeleccionTemperatura);
-                return res.setScale(2, RoundingMode.FLOOR);
-            case "Celcius (°C) a Kelvin (°K)":
-                res = valorUsuario.add(new BigDecimal("273.15"));
-                mensajeResultadoConversion(valorUsuario, res, valorSeleccionTemperatura);
-                return res.setScale(2, RoundingMode.FLOOR);
-            case "Farenheit (°F) a Celsius (°C)":
-                res = valorUsuario.subtract(new BigDecimal("32")).divide(new BigDecimal("1.8"), 2, RoundingMode.FLOOR);
-                mensajeResultadoConversion(valorUsuario, res, valorSeleccionTemperatura);
-                return res.setScale(2, RoundingMode.FLOOR);
-            case "Farenheit (°F) a Kelvin (°K)":
-                res = valorUsuario.subtract(new BigDecimal("32")).multiply(new BigDecimal("5")).divide(
-                        new BigDecimal("9"), 2, RoundingMode.FLOOR).add(new BigDecimal("273.15"));
-                mensajeResultadoConversion(valorUsuario, res, valorSeleccionTemperatura);
-                return res.setScale(2, RoundingMode.FLOOR);
-            case "Kelvin (°K) a Celsius (°C)":
-                res = valorUsuario.subtract(new BigDecimal("273.15"));
-                mensajeResultadoConversion(valorUsuario, res, valorSeleccionTemperatura);
-                return res.setScale(2, RoundingMode.FLOOR);
-            case "Kelvin (°K) a Farenheit (°F)":
-                res = valorUsuario.subtract(new BigDecimal("273.15")).multiply(
-                        new BigDecimal("1.8")).add(new BigDecimal("32"));
-                mensajeResultadoConversion(valorUsuario, res, valorSeleccionTemperatura);
-                return res.setScale(2, RoundingMode.FLOOR);
-            default:
-                return null;
+    public Double compute(Double base) {
+        if (base < Double.MIN_VALUE || base > Double.MAX_VALUE || base.isNaN()) {
+            throw new IllegalArgumentException("Base value out of boundary.");
         }
-    }
-
-    public void mensajeResultadoConversion(BigDecimal valor, BigDecimal resultado, String valorSeleccionTemp) {
-        JOptionPane.showMessageDialog(null, valor + "° " + valorSeleccionTemp
-                + ": " + resultado.setScale(2, RoundingMode.FLOOR) + "°");
-    }
-
-    /**
-     * @return the escalaCelsius
-     */
-    public BigDecimal getEscalaCelsius() {
-        return escalaCelsius;
-    }
-
-    /**
-     * @param escalaCelsius the escalaCelsius to set
-     */
-    public void setEscalaCelsius(BigDecimal escalaCelsius) {
-        this.escalaCelsius = escalaCelsius;
-    }
-
-    /**
-     * @return the escalaFarenheit
-     */
-    public BigDecimal getEscalaFarenheit() {
-        return escalaFarenheit;
-    }
-
-    /**
-     * @param escalaFarenheit the escalaFarenheit to set
-     */
-    public void setEscalaFarenheit(BigDecimal escalaFarenheit) {
-        this.escalaFarenheit = escalaFarenheit;
-    }
-
-    /**
-     * @return the escalaKelvin
-     */
-    public BigDecimal getEscalaKelvin() {
-        return escalaKelvin;
-    }
-
-    /**
-     * @param escalaKelvin the escalaKelvin to set
-     */
-    public void setEscalaKelvin(BigDecimal escalaKelvin) {
-        this.escalaKelvin = escalaKelvin;
-    }
-
-    /**
-     * @return the SIMBOLO_CELSIUS
-     */
-    public static String getSIMBOLO_CELSIUS() {
-        return SIMBOLO_CELSIUS;
-    }
-
-    /**
-     * @return the SIMBOLO_FARENHEIT
-     */
-    public static String getSIMBOLO_FARENHEIT() {
-        return SIMBOLO_FARENHEIT;
-    }
-
-    /**
-     * @return the SIMBOLO_KELVIN
-     */
-    public static String getSIMBOLO_KELVIN() {
-        return SIMBOLO_KELVIN;
+        return Double.valueOf(
+                String.format(
+                        "%1$.2f",
+                        calculableTemperature.applyFormula(base)
+                )
+        );
     }
 }
