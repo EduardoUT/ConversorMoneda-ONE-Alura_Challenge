783ae065be544f0b9256af1ce7eaa86c55e371d5
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-04-16 16:50:02 -0600

    Clase para manejo de archivo properties.config

---
+++ src/main/java/io/github/eduardout/converter/currency/config/PropertiesConfig.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2025 EduardoUT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.eduardout.converter.currency.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * This class have two ways to handle key, value properties of a resource like a
+ * database, an API, or a concrete file in the resources package, this last one
+ * must be defined in the resources package and must have properties to read.
+ *
+ * When using a file must have the .properties extention.
+ *
+ * @author EduardoUT
+ */
+public class PropertiesConfig {
+
+    private Properties properties;
+    private String propertyKeyPrefix;
+
+    private PropertiesConfig(Properties properties, String propertyKeyPrefix) {
+        this.properties = properties;
+        this.propertyKeyPrefix = propertyKeyPrefix;
+    }
+
+    public static PropertiesConfig fromFile(String propertiesPath, String propertyKeyPrefix)
+            throws IOException {
+        if (propertiesPath == null || propertiesPath.isEmpty()) {
+            throw new IllegalArgumentException("Properties path cannot be null "
+                    + "or empty.");
+        }
+        if (propertyKeyPrefix == null || propertyKeyPrefix.isEmpty()) {
+            throw new IllegalArgumentException("The property key prefix is null or empty.");
+        }
+
+        try (InputStream input = PropertiesConfig.class.getClassLoader()
+                .getResourceAsStream(propertiesPath)) {
+            if (input == null) {
+                throw new FileNotFoundException("Properties file not found.");
+            }
+            Properties props = new Properties();
+            props.load(input);
+            if (props.isEmpty()) {
+                throw new IllegalStateException("Properties file cannot be left "
+                        + "empty.");
+            }
+            return new PropertiesConfig(props, propertyKeyPrefix);
+        }
+    }
+
+    public String getPropertyKeyPrefix() {
+        return propertyKeyPrefix;
+    }
+
+    public String[] getKeyProperties() {
+        return properties.keySet().stream()
+                .filter(key -> key.toString().startsWith(propertyKeyPrefix))
+                .map(key -> key.toString().replace(propertyKeyPrefix, ""))
+                .toArray(String[]::new);
+    }
+
+    public String getPropertyValue(String key) {
+        if (key == null || key.isEmpty()) {
+            throw new IllegalArgumentException("Property key provided is null "
+                    + "or empty.");
+        }
+        if (!properties.containsKey(propertyKeyPrefix + key)) {
+            throw new IllegalStateException("The given key " + key
+                    + " does not exists on the loaded properties file or "
+                    + "check prefix for " + propertyKeyPrefix);
+        }
+        String value = properties.getProperty(propertyKeyPrefix + key);
+        if (value.matches("^https?://.+")) {
+            validateUrlFormat(value, key);
+        }
+        return value;
+    }
+
+    private void validateUrlFormat(String url, String key) {
+        if (url != null && !url.matches("^https://.+")) {
+            throw new IllegalStateException(
+                    "Invalid URL for  '" + key + "': " + url
+            );
+        }
+    }
+}
