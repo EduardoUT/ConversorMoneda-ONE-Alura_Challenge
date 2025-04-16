701bfef1409c79e59d60efcd7dfbf8f18ce20a09
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-04-16 17:12:45 -0600

    Agregando pruebas para clase CurrencyUnit.java

--- src/test/java/io/github/eduardout/converter/currency/CurrencyUnitTest.java
+++ src/test/java/io/github/eduardout/converter/currency/CurrencyUnitTest.java
@@ -2,13 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package io.hithub.eduardout.converter.currency;
+package io.github.eduardout.converter.currency;
 
-import io.github.eduardout.converter.currency.CurrencyCodeIso4217;
-import io.github.eduardout.converter.currency.CurrencyUnit;
 import java.util.Currency;
 import java.util.Set;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,13 +17,13 @@ import org.junit.jupiter.params.provider.MethodSource;
  */
 class CurrencyUnitTest {
 
-    private CurrencyUnit currencyUnit = new CurrencyUnit(CurrencyCodeIso4217.USD);
+    private CurrencyUnit currencyUnit = new CurrencyUnit(ISO4217Currency.USD);
 
     @DisplayName("Debería aceptar códigos de divisa ISO 4217 válidos, "
             + "compatibles con Currency.")
     @MethodSource("currenciesEnumType")
     @ParameterizedTest
-    void testCompatibleCurrencies(CurrencyCodeIso4217 currencyCodeIso4217) {
+    void testCompatibleCurrencies(ISO4217Currency currencyCodeIso4217) {
         Currency enumCurrencyCode = Currency.getInstance(currencyCodeIso4217.toString());
         assertTrue(currencyUnit.getCurrencies().contains(enumCurrencyCode));
     }
@@ -37,7 +34,7 @@ class CurrencyUnitTest {
     }
 
     @SuppressWarnings("unused")
-    static CurrencyCodeIso4217[] currenciesEnumType() {
-        return CurrencyCodeIso4217.values();
+    static ISO4217Currency[] currenciesEnumType() {
+        return ISO4217Currency.values();
     }
 }
