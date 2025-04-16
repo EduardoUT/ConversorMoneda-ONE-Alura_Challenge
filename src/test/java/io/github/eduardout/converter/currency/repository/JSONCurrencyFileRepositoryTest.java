fe97f0354223782f96ed1bad4183f2e5591c67c8
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-04-16 17:23:55 -0600

    Agregando pruebas para clase JSONCurrencyFileRepositoryTest.java

---
+++ src/test/java/io/github/eduardout/converter/currency/repository/JSONCurrencyFileRepositoryTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2025 EduardoUT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.eduardout.converter.currency.repository;
+
+import io.github.eduardout.converter.currency.CurrencyUnit;
+import static io.github.eduardout.converter.currency.ISO4217Currency.*;
+import io.github.eduardout.converter.currency.repository.JSONCurrencyFileRepository;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.io.TempDir;
+
+/**
+ *
+ * @author EduardoUT
+ */
+public class JSONCurrencyFileRepositoryTest {
+
+    @TempDir
+    static Path tempDir;
+
+    private JSONCurrencyFileRepository jSONCurrencyFileRepository;
+    private Path testFilePath;
+    private CurrencyUnit baseCurrency;
+    private CurrencyUnit targetCurrency;
+
+    @SuppressWarnings("unused")
+    @BeforeEach
+    void setUp() throws IOException {
+        testFilePath = tempDir.resolve("test-rates.json");
+        baseCurrency = new CurrencyUnit(MXN);
+        targetCurrency = new CurrencyUnit(GBP);
+    }
+
+    @Test
+    void testReadWriteOperations() throws IOException {
+        jSONCurrencyFileRepository = new JSONCurrencyFileRepository(testFilePath.toString());
+        JSONObject testData = new JSONObject().put("mxn", new JSONObject().put("gbp", 0.85));
+        jSONCurrencyFileRepository.updateCurrencyRates(testData);
+        BigDecimal targetRate = jSONCurrencyFileRepository.getCurrencyRate(baseCurrency, targetCurrency);
+        assertEquals(new BigDecimal("0.85"), targetRate);
+    }
+
+    @DisplayName("Debería lanzar JSONException cuando los códigos de divisa no existan.")
+    @Test
+    void testReadInvalidCurrencyPair() throws IOException {
+        JSONException e;
+        JSONObject invalidData = new JSONObject().put("jpy", new JSONObject().put("usd", 0.75));
+        jSONCurrencyFileRepository = new JSONCurrencyFileRepository(testFilePath.toString());
+        jSONCurrencyFileRepository.updateCurrencyRates(invalidData);
+        e = assertThrowsExactly(JSONException.class, () -> {
+            jSONCurrencyFileRepository.getCurrencyRate(baseCurrency, targetCurrency);
+        });
+        assertEquals("JSONObject[\""
+                + baseCurrency.getCurrencyCode().toLowerCase()
+                + "\"] not found.", e.getMessage()
+        );
+    }
+
+    @DisplayName("Debería actualizar el repositorio solo cuando los datos del "
+            + "json sean diferentes.")
+    @Test
+    void testUpdateWhenThereIsNewData() throws IOException {
+        BigDecimal unexpected = new BigDecimal(0.25);
+        JSONObject currentData = new JSONObject().put("mxn", new JSONObject().put("gbp", 0.25));
+        JSONObject newData = new JSONObject().put("mxn", new JSONObject().put("gbp", 0.85));
+        jSONCurrencyFileRepository = new JSONCurrencyFileRepository(testFilePath.toString());
+        jSONCurrencyFileRepository.updateCurrencyRates(currentData);
+        jSONCurrencyFileRepository.updateCurrencyRates(currentData);
+        jSONCurrencyFileRepository.updateCurrencyRates(newData);
+        jSONCurrencyFileRepository.updateCurrencyRates(newData);
+        assertNotEquals(unexpected, jSONCurrencyFileRepository.getCurrencyRate(baseCurrency, targetCurrency));
+    }
+}
