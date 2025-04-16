10eabb8fd975bc9e2ac3d0ecc2b029c13fc101be
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-04-16 17:05:13 -0600

    Agregando interface y clase para formateo de divisas desde el API

---
+++ src/main/java/io/github/eduardout/converter/util/DefaultRateParser.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2025 EduardoUT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.eduardout.converter.util;
+
+import io.github.eduardout.converter.currency.CurrencyUnit;
+import java.math.BigDecimal;
+import org.json.JSONObject;
+
+/**
+ * This is an implementation to return the BigDecimal currency rate according to
+ * the body response of a JSONObject the Free Currency Exchange Rates API.
+ *
+ * @author EduardoUT
+ */
+public class DefaultRateParser implements RateParser {
+
+    @Override
+    public BigDecimal parseRate(JSONObject response, CurrencyUnit base, CurrencyUnit target) {
+        return response
+                .getJSONObject(base.getCurrencyCode().toLowerCase())
+                .getBigDecimal(target.getCurrencyCode().toLowerCase());
+    }
+}
---
+++ src/main/java/io/github/eduardout/converter/util/RateParser.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2025 EduardoUT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.eduardout.converter.util;
+
+import io.github.eduardout.converter.currency.CurrencyUnit;
+import java.math.BigDecimal;
+import org.json.JSONObject;
+
+/**
+ * Since API currency rate providers have different ways to show the concrete
+ * currency rate amount, you can create your own way to parse the response
+ * depending on the JSONObject response.
+ *
+ * @author EduardoUT
+ */
+public interface RateParser {
+
+    BigDecimal parseRate(JSONObject response, CurrencyUnit base, CurrencyUnit target);
+}
