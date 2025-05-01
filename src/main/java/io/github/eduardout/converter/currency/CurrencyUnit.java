563e95da5c101d05cc07208b52d263da1fa7b463
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-05-01 13:03:08 -0600

    Agregar licencia y sobreescritura de toString

--- src/main/java/io/github/eduardout/converter/currency/CurrencyUnit.java
+++ src/main/java/io/github/eduardout/converter/currency/CurrencyUnit.java
@@ -1,6 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2025 EduardoUT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package io.github.eduardout.converter.currency;
 
@@ -16,7 +28,7 @@ import java.util.Set;
  * From this class we can set and obtain the currency code, a numeric code and
  * its symbol.
  *
- * @author Eduardo Reyes Hernández
+ * @author EduardoUT
  */
 public class CurrencyUnit {
 
@@ -58,6 +70,11 @@ public class CurrencyUnit {
 
     public Set<Currency> getCurrencies() {
         return Currency.getAvailableCurrencies();
+    }
+
+    @Override
+    public String toString() {
+        return currency.getCurrencyCode() + " - " + currency.getDisplayName();
     }
 
     /**
