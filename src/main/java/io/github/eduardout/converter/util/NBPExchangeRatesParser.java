ede128f89ff0bf55d82b0e71fd0d0d3147ee19bf
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-05-08 16:00:54 -0600

    Modificar: Manejo de key en JSONArray

--- src/main/java/io/github/eduardout/converter/util/NBPExchangeRatesParser.java
+++ src/main/java/io/github/eduardout/converter/util/NBPExchangeRatesParser.java
@@ -1,4 +1,31 @@
 package io.github.eduardout.converter.util;
 
-public class NBPExchangeRatesParser {
+import io.github.eduardout.converter.currency.CurrencyUnit;
+import org.json.JSONArray;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class NBPExchangeRatesParser implements RateParser {
+
+    @Override
+    public Map<String, BigDecimal> parseRate(Object response, CurrencyUnit base, CurrencyUnit target) {
+        JSONArray jsonArray = new JSONArray(response.toString());
+        JSONArray rates = jsonArray.getJSONObject(0).getJSONArray("rates");
+        String currencyKey = "currency";
+        return rates.toList()
+                .stream()
+                .filter(HashMap.class::isInstance)
+                .map(HashMap.class::cast)
+                .filter(hashMap -> hashMap.containsValue(base.getCurrencyCode())
+                        || hashMap.containsValue(target.getCurrencyCode()))
+                .filter(hashMap -> hashMap.remove(currencyKey, hashMap.get(currencyKey)))
+                .collect(Collectors.toMap(
+                                key -> key.get("code").toString(),
+                                value -> new BigDecimal(value.get("mid").toString())
+                        )
+                );
+    }
 }
