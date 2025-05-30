aaf36862a92994f57598e9b10cebc42fe671a255
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-05-30 17:12:36 -0600

    Modificar: Reducción de argumentos acorde a interface RateParser, recibe ahora el response como String.

--- src/main/java/io/github/eduardout/converter/util/NBPExchangeRatesParser.java
+++ src/main/java/io/github/eduardout/converter/util/NBPExchangeRatesParser.java
@@ -1,6 +1,5 @@
 package io.github.eduardout.converter.util;
 
-import io.github.eduardout.converter.currency.CurrencyUnit;
 import org.json.JSONArray;
 
 import java.math.BigDecimal;
@@ -11,16 +10,14 @@ import java.util.stream.Collectors;
 public class NBPExchangeRatesParser implements RateParser {
 
     @Override
-    public Map<String, BigDecimal> parseRate(Object response, CurrencyUnit base, CurrencyUnit target) {
-        JSONArray jsonArray = new JSONArray(response.toString());
+    public Map<String, BigDecimal> parseRate(String response) {
+        JSONArray jsonArray = new JSONArray(response);
         JSONArray rates = jsonArray.getJSONObject(0).getJSONArray("rates");
         String currencyKey = "currency";
         return rates.toList()
                 .stream()
                 .filter(HashMap.class::isInstance)
                 .map(HashMap.class::cast)
-                .filter(hashMap -> hashMap.containsValue(base.getCurrencyCode())
-                        || hashMap.containsValue(target.getCurrencyCode()))
                 .filter(hashMap -> hashMap.remove(currencyKey, hashMap.get(currencyKey)))
                 .collect(Collectors.toMap(
                                 key -> key.get("code").toString(),
