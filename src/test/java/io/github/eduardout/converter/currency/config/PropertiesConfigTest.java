6a950d187068e2aa4444236514ad31e6f8fc2340
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-05-30 15:07:29 -0600

    Agregar: Casos de pruebas para prefijo.

--- src/test/java/io/github/eduardout/converter/currency/config/PropertiesConfigTest.java
+++ src/test/java/io/github/eduardout/converter/currency/config/PropertiesConfigTest.java
@@ -17,10 +17,14 @@
 package io.github.eduardout.converter.currency.config;
 
 import java.io.FileNotFoundException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
-import java.net.URISyntaxException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInstance;
 
@@ -33,91 +37,117 @@ class PropertiesConfigTest {
     private String propertiesKeyPrefix;
     private String propertiesPath;
 
+    @BeforeEach
     void setUpClass() throws IOException {
         propertiesKeyPrefix = "testp.";
         propertiesPath = "test.properties";
-        propertiesConfig = PropertiesConfig.fromFile(propertiesPath, propertiesKeyPrefix);
+        propertiesConfig = PropertiesConfig.fromFile(propertiesPath);
     }
 
     @Test
-    void fromFile_NullOrEmptyArguments_ThrowsException()
-            throws IOException {
-        setUpClass();
+    void fromFile_NullOrEmptyArguments_ThrowsException() {
         String messagePathException = "Properties path cannot be null "
                 + "or empty.";
-        String messagePrefixException = "The property key prefix is null or empty.";
-        argumentException = assertThrowsExactly(IllegalArgumentException.class, () -> {
-            propertiesConfig = PropertiesConfig.fromFile(null, propertiesKeyPrefix);
-        });
+        propertiesPath = null;
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig = PropertiesConfig.fromFile(propertiesPath)
+        );
         assertEquals(messagePathException, argumentException.getMessage());
-        argumentException = assertThrowsExactly(IllegalArgumentException.class, () -> {
-            propertiesConfig = PropertiesConfig.fromFile("", propertiesKeyPrefix);
-        });
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig = PropertiesConfig.fromFile("")
+        );
         assertEquals(messagePathException, argumentException.getMessage());
-        argumentException = assertThrowsExactly(IllegalArgumentException.class, () -> {
-            propertiesConfig = PropertiesConfig.fromFile(propertiesPath, null);
-        });
-        assertEquals(messagePrefixException, argumentException.getMessage());
-        argumentException = assertThrowsExactly(IllegalArgumentException.class, () -> {
-            propertiesConfig = PropertiesConfig.fromFile(propertiesPath, "");
-        });
-        assertEquals(messagePrefixException, argumentException.getMessage());
     }
 
     @Test
-    void fromFileValidPrefix() throws IOException {
-        setUpClass();
-        assertEquals("https://firstkey.com", propertiesConfig.getPropertyValue("first.key"));
-    }
-
-    @Test
-    void fromFileNotFound_ThrowsException() throws IOException {
+    void fromFileNotFound_ThrowsException() {
         FileNotFoundException notFoundException;
-        notFoundException = assertThrowsExactly(FileNotFoundException.class, () -> {
-            propertiesConfig = PropertiesConfig.fromFile("not_exist.properties", "random.");
-        });
+        notFoundException = assertThrowsExactly(FileNotFoundException.class,
+                () -> propertiesConfig = PropertiesConfig.fromFile("not_exist.properties")
+        );
         assertEquals("Properties file not found.", notFoundException.getMessage());
     }
 
     @DisplayName("Debería lanzar IllegalArgumentException si el key o value "
             + "son null o vacíos.")
     @Test
-    void fromFileEmptyProperties_ThrowsException() throws IOException {
+    void fromFileEmptyProperties_ThrowsException() {
         String expectedMessage = "Properties file cannot be left empty.";
-        stateException = assertThrowsExactly(IllegalStateException.class, () -> {
-            propertiesConfig = PropertiesConfig.fromFile("intentionalEmptyFile.properties", "some.");
-        });
+        stateException = assertThrowsExactly(IllegalStateException.class,
+                () -> propertiesConfig = PropertiesConfig.fromFile("intentionalEmptyFile.properties")
+        );
         assertEquals(expectedMessage, stateException.getMessage());
     }
 
-    @DisplayName("Debería lanzar IllegalArgumentException para un key,"
-            + "null o vacío.")
     @Test
-    void getPropertyValueNullOrEmpty_ThrowsException() throws IOException {
-        String expectedMessage = "Property key provided is null or empty.";
-        setUpClass();
-        argumentException = assertThrowsExactly(IllegalArgumentException.class, () -> {
-            propertiesConfig.getPropertyValue(null);
-        });
-        assertEquals(expectedMessage, argumentException.getMessage());
-        argumentException = assertThrowsExactly(IllegalArgumentException.class, () -> {
-            propertiesConfig.getPropertyValue("");
-        });
-        assertEquals(expectedMessage, argumentException.getMessage());
+    void getPropertyValueValidArguments() {
+        assertEquals("https://firstkey.com", propertiesConfig.getPropertyValue(propertiesKeyPrefix, "first.key"));
+    }
+
+    @DisplayName("Debería lanzar IllegalArgumentException para un key "
+            + "null o vacío")
+    @Test
+    void getPropertyValueInvalidArguments_ThrowsException() {
+        String keyInvalidMessage = "Property key provided is null or empty.";
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getPropertyValue(propertiesKeyPrefix, null)
+        );
+        assertEquals(keyInvalidMessage, argumentException.getMessage());
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getPropertyValue(propertiesKeyPrefix, "")
+        );
+        assertEquals(keyInvalidMessage, argumentException.getMessage());
+    }
+
+    @DisplayName("Debería lanzar IllegalArgumentException para el argumento propertyKeyPrefix " +
+            "null o vacío y verificar si finaliza con un punto")
+    @Test
+    void testPropertyKeyPrefix() {
+        String badPropertyKeyPrefix = "testp-";
+        String prefixInvalidMessageOne = "The property key prefix is null or empty.";
+        String prefixInvalidMessageTwo = "The property key prefix must end with a point " +
+                "character the given was: " + badPropertyKeyPrefix;
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getPropertyValue(null, "first.key")
+        );
+        assertEquals(prefixInvalidMessageOne, argumentException.getMessage());
+
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getPropertyValue("", "first.key")
+        );
+        assertEquals(prefixInvalidMessageOne, argumentException.getMessage());
+
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getPropertyValue(badPropertyKeyPrefix, "first.key"));
+        assertEquals(prefixInvalidMessageTwo, argumentException.getMessage());
+
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getKeyProperties(null)
+        );
+        assertEquals(prefixInvalidMessageOne, argumentException.getMessage());
+
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getKeyProperties("")
+        );
+        assertEquals(prefixInvalidMessageOne, argumentException.getMessage());
+
+        argumentException = assertThrowsExactly(IllegalArgumentException.class,
+                () -> propertiesConfig.getKeyProperties(badPropertyKeyPrefix)
+        );
+        assertEquals(prefixInvalidMessageTwo, argumentException.getMessage());
     }
 
     @DisplayName("Debería lanzar IllegalStateException cuando la propiedad no "
             + "sea una de las asignadas en el key Set")
     @Test
-    void getPropertyValueNotInKeySet_ThrowsException() throws IOException, URISyntaxException {
-        setUpClass();
+    void getPropertyValueNotInKeySet_ThrowsException() {
         String notAKey = "not.exist";
-        stateException = assertThrowsExactly(IllegalStateException.class, () -> {
-            propertiesConfig.getPropertyValue(notAKey);
-        });
+        stateException = assertThrowsExactly(IllegalStateException.class,
+                () -> propertiesConfig.getPropertyValue(propertiesKeyPrefix, notAKey)
+        );
         assertEquals("The given key " + notAKey
-                + " does not exists on the loaded properties file or "
-                            + "check prefix for " + propertiesKeyPrefix, 
+                        + " does not exists on the loaded properties file or "
+                        + "check prefix for " + propertiesKeyPrefix,
                 stateException.getMessage()
         );
     }
@@ -126,15 +156,14 @@ class PropertiesConfigTest {
             + "con el formato esperado.")
     @Test
     void getPropertyValueInvalidUrlFormat() throws IOException {
-        setUpClass();
         String key = "url";
         String invalidUrlValue = "http://example.com";
         String expectedMessage = "Invalid URL for  '" + key + "': "
                 + invalidUrlValue;
-        propertiesConfig = PropertiesConfig.fromFile(propertiesPath, "invalid.");
-        stateException = assertThrowsExactly(IllegalStateException.class, () -> {
-            propertiesConfig.getPropertyValue(key);
-        });
+        propertiesConfig = PropertiesConfig.fromFile(propertiesPath);
+        stateException = assertThrowsExactly(IllegalStateException.class,
+                () -> propertiesConfig.getPropertyValue("invalid.", key)
+        );
         assertEquals(expectedMessage, stateException.getMessage());
     }
 }
