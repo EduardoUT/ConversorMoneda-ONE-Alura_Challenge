03914b0d6ad2e870003219215b128065bbc8f495
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-05-30 15:09:51 -0600

    Agregar: Clase base para clases de tipo RateProvider

--- src/main/java/io/github/eduardout/converter/currency/provider/AbstractCurrencyProvider.java
+++ src/main/java/io/github/eduardout/converter/currency/provider/AbstractCurrencyProvider.java
@@ -1,4 +1,39 @@
 package io.github.eduardout.converter.currency.provider;
 
-public class AbstractCurrencyProvider {
+import io.github.eduardout.converter.currency.config.PropertiesConfig;
+
+public abstract class AbstractCurrencyProvider implements RateProvider {
+
+    private HttpClient httpClient;
+    private PropertiesConfig propertiesConfig;
+    private String propertyKeyPrefix;
+
+    protected AbstractCurrencyProvider (HttpClient httpClient,
+                                     PropertiesConfig propertiesConfig,
+                                     String propertyKeyPrefix) {
+        if(httpClient == null) {
+            throw new IllegalArgumentException("HttpClient is null");
+        }
+        if(propertiesConfig == null) {
+            throw new IllegalArgumentException("PropertiesConfig is null.");
+        }
+        if(propertyKeyPrefix == null || propertyKeyPrefix.isEmpty()) {
+            throw new IllegalArgumentException("The property key prefix is null or empty.");
+        }
+        this.httpClient = httpClient;
+        this.propertiesConfig = propertiesConfig;
+        this.propertyKeyPrefix = propertyKeyPrefix;
+    }
+
+    public HttpClient getHttpClient() {
+        return httpClient;
+    }
+
+    public PropertiesConfig getPropertiesConfig() {
+        return propertiesConfig;
+    }
+
+    public String getPropertyKeyPrefix() {
+        return propertyKeyPrefix;
+    }
 }
