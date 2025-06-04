5d472f067ca78720cacd381837783f39c236b3e5
Eduardo Reyes Hernández <mcorey316@gmail.com>
2025-06-04 14:17:29 -0600

    Eliminar: Clases importadas sin uso

--- src/main/java/io/github/eduardout/converter/currency/provider/HttpClient.java
+++ src/main/java/io/github/eduardout/converter/currency/provider/HttpClient.java
@@ -23,8 +23,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 /**
  * @author EduardoUT
